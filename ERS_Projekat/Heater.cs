4c6bcebb846f6dd1964dcaf70b8e3b17780dc81e
Vladislav Petković <vladislav.petkovic.02@gmail.com>
2023-12-04 22:05:36 +0100

    Adding classes and interfaces
    I'm adding basic classes for further progress in app development (classes are NOT USABLE YET)

---
+++ ERS_Projekat/Heater.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat
+{
+    internal class Heater : IHeater
+    {
+
+        int temperature;
+        DateTime startTime;
+        TimeSpan elapsedTime;
+        double fuelUsed;
+        
+        public int CheckTemperature()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetHeaterDetails()
+        {
+            return "Start time: [" + startTime.ToString() + "]; On-time: [" + elapsedTime + "]; Fuel used: "+ fuelUsed.ToString();
+        }
+
+        public bool TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}

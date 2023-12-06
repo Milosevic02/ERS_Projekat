e960157b582a8b2ae6ea9f846e617fa96daf8a46
Dragan <milosevicdragan002@gmail.com>
2023-12-06 10:48:30 +0100

    Create Regulator.cs

---
+++ ERS_Projekat/Regulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat
+{
+    internal class Regulator : IRegulator
+    {
+        bool mode;
+        double dayTemperature;
+        double nightTemperature;
+        double dayStart;
+        double dayEnd;
+
+        public bool Settings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ChangeTemperature()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendHeaterIsOn()
+        {
+            throw new NotImplementedException();
+        }
+        public bool SaveEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+    }
+}

d81a518d728596e5fb4c73c2e50369b1c97746bc
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2023-12-13 15:17:42 +0100

    Create FunctionHandler.cs

---
+++ ERS_Projekat/FunctionHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat
+{
+    internal class FunctionHandler : IFunctionHandler
+    {
+        static int devId = 0;
+
+        Heater heater = null;
+
+        public FunctionHandler()
+        {
+            InitializeHeater();
+        }
+
+        readonly string logFilePath = "log.txt";
+
+        internal Heater Heater { get => heater; set => heater = value; }
+
+        public bool ChangeFuel(Heater h, double newConstant)
+        {
+            h.FuelConstant = newConstant; 
+            return true;
+        }
+
+        public bool ClearLogFile()
+        {
+            try
+            {
+                File.WriteAllText(logFilePath, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error clearing log file: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public Device InitializeDevice()
+        {
+            Device d = new Device(++devId);
+            return d;
+        }
+
+        public bool InitializeHeater()
+        {
+            Heater = new Heater(1);
+            return true;
+        }
+
+        public bool InitializeRegulator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenLog()
+        {
+            try
+            {
+                Process.Start("notepad.exe", logFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error opening file: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public bool SelectTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

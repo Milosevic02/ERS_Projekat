a4ed9ed761c0dd60168658771a1a32e2ab1208e1
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2023-12-13 15:17:38 +0100

    Create IFunctionHandler.cs

---
+++ ERS_Projekat/IFunctionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat
+{
+    internal interface IFunctionHandler
+    {
+        bool InitializeRegulator();
+        Device InitializeDevice();
+        bool InitializeHeater();
+
+        bool SelectTime();
+        bool OpenLog();
+        bool ClearLogFile();
+        bool ChangeFuel(Heater h, double newConst);
+    }
+}

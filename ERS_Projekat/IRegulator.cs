8fb06b2d54935da95f0f5eb43a53a2a0dd8785ac
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2023-12-19 16:51:39 +0100

    Create IRegulator.cs

---
+++ ERS_Projekat/IRegulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat
+{
+    internal interface IRegulator
+    {
+        bool Settings(); ///UI za podesavanje
+        bool TemperatureControl(); ///Promena temperature u centralnoj peci
+
+        bool SendHeaterIsOn(); ///Informacija da je pec pocela sa radom
+
+        bool SaveEvent(double avgTemp,bool on); // Cuvanje svih dogadjaja u txt fajl
+
+        bool AddDevice(Device d);
+
+        bool RemoveDevice(Device d);
+
+
+
+
+    }
+}

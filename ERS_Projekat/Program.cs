a8624e36a4a300d6dab88301c334f5965a55d446
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2023-12-19 16:51:30 +0100

    Update Program.cs

--- ERS_Projekat/Program.cs
+++ ERS_Projekat/Program.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ERS_Projekat
 {
     internal class Program
     {
-        static void Main()
-        {
-            UIHandler handler = new UIHandler();
-            while(true)
-            {
-                handler.Menu();
-                handler.GetCommand();
-            }
-
+        static void Main(string[] args)
+        {          
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
